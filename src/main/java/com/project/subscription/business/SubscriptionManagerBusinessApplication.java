6a7316ca6954e6a94ff5a808a76ced7beabd0104
nathan <nathan960307@gmail.com>
2026-02-02 19:48:40 +0900

    init: setup business server with ddd-lite skeleton

---
+++ src/main/java/com/project/subscription/business/SubscriptionManagerBusinessApplication.java
@@ -0,0 +1,13 @@
+package com.project.subscription.business;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubscriptionManagerBusinessApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SubscriptionManagerBusinessApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/subscription/business/SubscriptionManagerBusinessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.subscription.business;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubscriptionManagerBusinessApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
