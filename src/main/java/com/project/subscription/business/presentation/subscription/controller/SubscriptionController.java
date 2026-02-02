f52a0cf2d8523264562547e99cb4926232751ba3
nathan <nathan960307@gmail.com>
2026-02-02 23:34:02 +0900

    feat(subscription): 구독 컨트롤러에서 서비스 연동 스켈레톤 구성 (#1)

--- src/main/java/com/project/subscription/business/presentation/subscription/controller/SubscriptionController.java
+++ src/main/java/com/project/subscription/business/presentation/subscription/controller/SubscriptionController.java
@@ -1,9 +1,49 @@
 package com.project.subscription.business.presentation.subscription.controller;
 
+import com.project.subscription.business.application.subscription.service.SubscriptionService;
+import com.project.subscription.business.presentation.subscription.dto.internal.SubscriptionInternalDto;
+import com.project.subscription.business.presentation.subscription.dto.response.SubscriptionDetailResponse;
+import com.project.subscription.business.presentation.subscription.dto.response.SubscriptionListResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/subscriptions")
 public class SubscriptionController {
+
+    @Autowired
+    private SubscriptionService subscriptionService;
+
+    // 구독 상세 조회
+    @GetMapping("/{id}")
+    public SubscriptionDetailResponse getSubscriptionDetail(@PathVariable Long id) {
+
+        SubscriptionInternalDto subscription = subscriptionService.getSubscriptionDetail(id);
+
+        // subscription 을 SubscriptionResponse로 포장 필요
+
+        return null;
+    }
+
+    // 내 구독 목록 조회
+    @GetMapping("/me")
+    public SubscriptionListResponse getMySubscriptions() {
+
+        List<SubscriptionInternalDto> subscriptions = subscriptionService.getMySubscriptions();
+
+        // subscriptions 을 SubscriptionResponse로 포장 필요
+
+
+        return null;
+
+    }
+
+
+
+
 }
