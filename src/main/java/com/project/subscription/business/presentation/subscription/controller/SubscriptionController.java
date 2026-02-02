094b260fe25eb3a3067f1b747966378315712181
nathan <nathan960307@gmail.com>
2026-02-02 22:45:51 +0900

    feat(subscription): 구독 컨트롤러 스켈레톤 추가 (#1) [2026-02-02]

---
+++ src/main/java/com/project/subscription/business/presentation/subscription/controller/SubscriptionController.java
@@ -0,0 +1,9 @@
+package com.project.subscription.business.presentation.subscription.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/subscriptions")
+public class SubscriptionController {
+}
