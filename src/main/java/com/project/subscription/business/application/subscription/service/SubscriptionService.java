fda0159dd13c4f833c91cb3d87110918a039e1ee
nathan <nathan960307@gmail.com>
2026-02-02 23:14:42 +0900

    feat(subscription): 구독 조회 서비스 메서드 시그니처 추가 (#1) [2026-02-02]

--- src/main/java/com/project/subscription/business/application/subscription/service/SubscriptionService.java
+++ src/main/java/com/project/subscription/business/application/subscription/service/SubscriptionService.java
@@ -1,7 +1,19 @@
 package com.project.subscription.business.application.subscription.service;
 
+import com.project.subscription.business.presentation.subscription.dto.internal.SubscriptionInternalDto;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class SubscriptionService {
+
+    public List<SubscriptionInternalDto> getMySubscriptions() { // 구독 서비스 목록 조회
+        return null;
+    }
+
+    public SubscriptionInternalDto getSubscriptionDetail(Long subscriptionId) { // 구독 서비스 단일 조회
+        return null;
+    }
+
 }
