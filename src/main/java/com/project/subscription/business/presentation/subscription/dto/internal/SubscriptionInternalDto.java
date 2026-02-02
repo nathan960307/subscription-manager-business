b8a9a7bd1b4aa56bcaca95cf833ba12a0b8ac1fd
nathan <nathan960307@gmail.com>
2026-02-02 23:05:54 +0900

    feat(subscription): 내부 변환용 SubscriptionInternalDto 스켈레톤 추가 (#1) [2026-02-02]

---
+++ src/main/java/com/project/subscription/business/presentation/subscription/dto/internal/SubscriptionInternalDto.java
@@ -0,0 +1,4 @@
+package com.project.subscription.business.presentation.subscription.dto.internal;
+
+public class SubscriptionInternalDto { //구독 서비스 단일 조회 DTO ( entity -> response dto 변환용 내부 DTO)
+}
