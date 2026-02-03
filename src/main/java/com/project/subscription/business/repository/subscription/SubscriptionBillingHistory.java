b11380a4827a1b58897090625c6dd73fb479ee1b
nathan <nathan960307@gmail.com>
2026-02-03 15:12:41 +0900

    feat(subscription): SubscriptionBillingHistory Repository 생성 (#1) [2026-02-03]

---
+++ src/main/java/com/project/subscription/business/repository/subscription/SubscriptionBillingHistory.java
@@ -0,0 +1,13 @@
+package com.project.subscription.business.repository.subscription;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+// 구독 이력 조회
+public interface SubscriptionBillingHistory extends JpaRepository<SubscriptionBillingHistory,Long> {
+
+    // 특정 구독의 모든 Billing History를 조회 하여 날짜 순으로 내림차순으로 정렬(최신순)
+    List<SubscriptionBillingHistory> findBySubscriptionIdOrderByBillingDateDesc(Long subscriptionId);
+
+}
