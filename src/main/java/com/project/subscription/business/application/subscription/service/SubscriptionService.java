6734ee688838ab416f802bc0f46fb6f9727d38c5
nathan <nathan960307@gmail.com>
2026-02-03 14:48:38 +0900

    feat(subscription): 구독 상세 조회 서비스 구현 (#1) [2026-02-03]

--- src/main/java/com/project/subscription/business/application/subscription/service/SubscriptionService.java
+++ src/main/java/com/project/subscription/business/application/subscription/service/SubscriptionService.java
@@ -1,19 +1,47 @@
 package com.project.subscription.business.application.subscription.service;
 
+import com.project.subscription.business.domain.subscription.entity.Subscription;
 import com.project.subscription.business.presentation.subscription.dto.internal.SubscriptionInternalDto;
+import com.project.subscription.business.repository.subscription.SubscriptionRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class SubscriptionService {
+
+    private final SubscriptionRepository subscriptionRepository; // 구독 repository
+
+    public SubscriptionInternalDto getSubscriptionDetail(Long subscriptionId) { // 구독 서비스 단일 조회
+        // 구독 ID 로 해당 서비스 상세 조회
+        Subscription subscription =subscriptionRepository.findById(subscriptionId)
+                .orElseThrow(() -> new IllegalArgumentException("구독이 존재하지 않습니다."));
+
+        // entity -> dto 변환
+        // 1. 생성자 방식
+        // 2. setter 방식
+        // 3. 빌더 방식
+
+        SubscriptionInternalDto subscriptionInternalDto = SubscriptionInternalDto.builder()
+                .id(subscription.getId())
+                .userId(subscription.getUserId())
+                .serviceId(subscription.getServiceId())
+                .price(subscription.getPrice())
+                .billingCycle(subscription.getBillingCycle())
+                .status(subscription.getStatus())
+                .nextBillingDate(subscription.getNextBillingDate())
+                .autoRenew(subscription.isAutoRenew())
+                .build();
+
+        return subscriptionInternalDto;
+    }
 
     public List<SubscriptionInternalDto> getMySubscriptions() { // 구독 서비스 목록 조회
         return null;
     }
 
-    public SubscriptionInternalDto getSubscriptionDetail(Long subscriptionId) { // 구독 서비스 단일 조회
-        return null;
-    }
+
 
 }
