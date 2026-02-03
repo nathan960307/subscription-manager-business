efedd4fcd512cc41b978274681eed2879e835ec6
nathan <nathan960307@gmail.com>
2026-02-03 14:47:10 +0900

    feat(subscription): Internal DTO 필드 채움 [2026-02-03]

--- src/main/java/com/project/subscription/business/presentation/subscription/dto/internal/SubscriptionInternalDto.java
+++ src/main/java/com/project/subscription/business/presentation/subscription/dto/internal/SubscriptionInternalDto.java
@@ -1,4 +1,21 @@
 package com.project.subscription.business.presentation.subscription.dto.internal;
 
+import lombok.Builder;
+import lombok.Getter;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Getter
+@Builder
 public class SubscriptionInternalDto { //구독 서비스 단일 조회 DTO ( entity -> response dto 변환용 내부 DTO)
+
+    private Long id;
+    private Long userId;
+    private Long serviceId;
+    private BigDecimal price;
+    private String billingCycle;
+    private String status;
+    private LocalDateTime nextBillingDate;
+    private boolean autoRenew;
 }
