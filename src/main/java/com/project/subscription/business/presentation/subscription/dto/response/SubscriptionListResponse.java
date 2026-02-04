f190570079f1df5e9d9aa699a8388557d6ef25c0
nathan <nathan960307@gmail.com>
2026-02-04 16:04:06 +0900

    feat(subscription): Subscription 구독 서비스 목록 출력을위한 response 완성(#1) [2026-02-04]

--- src/main/java/com/project/subscription/business/presentation/subscription/dto/response/SubscriptionListResponse.java
+++ src/main/java/com/project/subscription/business/presentation/subscription/dto/response/SubscriptionListResponse.java
@@ -1,4 +1,25 @@
 package com.project.subscription.business.presentation.subscription.dto.response;
 
+import com.project.subscription.business.presentation.subscription.dto.internal.SubscriptionInternalDto;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.time.OffsetDateTime;
+import java.util.List;
+
+@Getter
+@AllArgsConstructor
 public class SubscriptionListResponse { // 구독 서비스 목록 출력용
+
+    private final String message;
+    private final OffsetDateTime timestamp;
+    private final List<SubscriptionInternalDto> data;
+
+    public static SubscriptionListResponse success(List<SubscriptionInternalDto> subscriptions) {
+        return new SubscriptionListResponse(
+                "구독 목록 조회 성공",
+                OffsetDateTime.now(),
+                subscriptions
+        );
+    }
 }
