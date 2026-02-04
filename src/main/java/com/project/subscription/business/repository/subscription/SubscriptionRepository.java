e8f121a55e392b1c1642f3929c5d37002a27027c
nathan <nathan960307@gmail.com>
2026-02-04 15:43:58 +0900

    feat(subscription): Subscription Repository 사용자별 구독 서비스 목록 조회 method 추가 (#1) [2026-02-04]

--- src/main/java/com/project/subscription/business/repository/subscription/SubscriptionRepository.java
+++ src/main/java/com/project/subscription/business/repository/subscription/SubscriptionRepository.java
@@ -3,5 +3,10 @@ package com.project.subscription.business.repository.subscription;
 import com.project.subscription.business.domain.subscription.entity.Subscription;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface SubscriptionRepository extends JpaRepository<Subscription,Long> {
+
+    // 사용자 별 구독 서비스 목록 조회
+    List<Subscription> findByUserId(Long userId);
 }
