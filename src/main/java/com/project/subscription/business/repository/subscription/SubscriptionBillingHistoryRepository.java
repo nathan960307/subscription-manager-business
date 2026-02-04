56e55c4dd69bc609dcd83c211f4e47ff4af6186e
nathan <nathan960307@gmail.com>
2026-02-04 17:52:08 +0900

    fix(subscription): Subscription 관련 Repository JPA miss match 오류 수정(#1) [2026-02-04]

--- src/main/java/com/project/subscription/business/repository/subscription/SubscriptionBillingHistoryRepository.java
+++ src/main/java/com/project/subscription/business/repository/subscription/SubscriptionBillingHistoryRepository.java
@@ -1,17 +1,18 @@
 package com.project.subscription.business.repository.subscription;
 
+import com.project.subscription.business.domain.subscription.entity.SubscriptionBillingHistory;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 // 구독 결제 이력 조회
-public interface SubscriptionBillingHistoryRepository extends JpaRepository<SubscriptionBillingHistoryRepository,Long> {
+public interface SubscriptionBillingHistoryRepository extends JpaRepository<SubscriptionBillingHistory,Long> {
 
     // 특정 구독의 모든 Billing History를 조회 하여 날짜 순으로 내림차순으로 정렬(최신순)
-    List<SubscriptionBillingHistoryRepository> findBySubscriptionIdOrderByBillingDateDesc(Long subscriptionId);
+    List<SubscriptionBillingHistory> findBySubscriptionIdOrderByBillingDateDesc(Long subscriptionId);
 
     // 과거순(오름차순) 조회
-    List<SubscriptionBillingHistoryRepository> findBySubscriptionIdOrderByBillingDateAsc(Long subscriptionId);
+    List<SubscriptionBillingHistory> findBySubscriptionIdOrderByBillingDateAsc(Long subscriptionId);
 
 
 }
--- src/main/java/com/project/subscription/business/repository/subscription/SubscriptionChangeHistoryRepository.java
+++ src/main/java/com/project/subscription/business/repository/subscription/SubscriptionChangeHistoryRepository.java
@@ -1,14 +1,15 @@
 package com.project.subscription.business.repository.subscription;
 
+import com.project.subscription.business.domain.subscription.entity.SubscriptionChangeHistory;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface SubscriptionChangeHistoryRepository extends JpaRepository<SubscriptionChangeHistoryRepository, Long> {
+public interface SubscriptionChangeHistoryRepository extends JpaRepository<SubscriptionChangeHistory, Long> {
 
     // 구독 변경 내역 리스트 최신순 조회
-    List<SubscriptionChangeHistoryRepository> findBySubscriptionIdOrderByChangedAtDesc(Long subscriptionId);
+    List<SubscriptionChangeHistory> findBySubscriptionIdOrderByChangedAtDesc(Long subscriptionId);
 
     // 과거순
-    List<SubscriptionChangeHistoryRepository> findBySubscriptionIdOrderByChangedAtAsc(Long subscriptionId);
+    List<SubscriptionChangeHistory> findBySubscriptionIdOrderByChangedAtAsc(Long subscriptionId);
 }
