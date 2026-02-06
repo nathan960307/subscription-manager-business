5bdd55e8f223e0a77ac412e99f38a0fa97a6b0ab
nathan <nathan960307@gmail.com>
2026-02-06 10:12:51 +0900

    feat(subscription): 구독 결제 내역 조회 controller 완성(#1) [2026-02-06]

--- src/main/java/com/project/subscription/business/presentation/subscription/controller/SubscriptionController.java
+++ src/main/java/com/project/subscription/business/presentation/subscription/controller/SubscriptionController.java
@@ -2,8 +2,10 @@ package com.project.subscription.business.presentation.subscription.controller;
 
 import com.project.subscription.business.application.subscription.service.SubscriptionService;
 import com.project.subscription.business.domain.subscription.entity.SubscriptionChangeHistory;
+import com.project.subscription.business.presentation.subscription.dto.internal.SubscriptionBillingHistoryInternalDto;
 import com.project.subscription.business.presentation.subscription.dto.internal.SubscriptionChangeHistoryInternalDto;
 import com.project.subscription.business.presentation.subscription.dto.internal.SubscriptionInternalDto;
+import com.project.subscription.business.presentation.subscription.dto.response.SubscriptionBillingHistoryListResponse;
 import com.project.subscription.business.presentation.subscription.dto.response.SubscriptionChangeHistoryListResponse;
 import com.project.subscription.business.presentation.subscription.dto.response.SubscriptionDetailResponse;
 import com.project.subscription.business.presentation.subscription.dto.response.SubscriptionListResponse;
@@ -49,7 +51,7 @@ public class SubscriptionController {
 
     }
 
-    // 구독 결제 내역 조회
+    // 구독 변경 내역 조회
     @GetMapping("/{subscriptionId}/changes")
     public SubscriptionChangeHistoryListResponse getMySubscriptions(@PathVariable Long subscriptionId) {
 
@@ -63,7 +65,19 @@ public class SubscriptionController {
 
     }
 
-    // 구독 변경 내역 조회
+    // 구독 결제 내역 조회
+    @GetMapping("/{subscriptionId}/billings")
+    public SubscriptionBillingHistoryListResponse getMySubscriptions(@PathVariable Long subscriptionId) {
+
+        Long userId = 1L; // TODO: 인증 도입 후 실제 userId로 교체
+
+        List<SubscriptionBillingHistoryInternalDto> subscriptions = subscriptionService.getMySubscriptionBillingHistories(userId,subscriptionId);
+
+        SubscriptionBillingHistoryListResponse subscriptionBillingHistoryListResponse = SubscriptionBillingHistoryListResponse.success(subscriptions);
+
+        return subscriptionBillingHistoryListResponse;
+
+    }
 
 
 
