18298e9345603524de5e7bd4a8134a04b4e47d22
nathan <nathan960307@gmail.com>
2026-02-06 09:41:58 +0900

    feat(subscription): 구독 결제 내역 내부 DTO 생성 및 entity 변환 정적 메서드 추가(#1) [2026-02-06]

---
+++ src/main/java/com/project/subscription/business/presentation/subscription/dto/internal/SubscriptionBillingHistoryInternalDto.java
@@ -0,0 +1,41 @@
+package com.project.subscription.business.presentation.subscription.dto.internal;
+
+import com.project.subscription.business.domain.subscription.entity.SubscriptionBillingHistory;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter
+@Builder
+public class SubscriptionBillingHistoryInternalDto {
+    private Long billingHistoryId;
+    private LocalDateTime billingDate;
+
+    private LocalDateTime billingPeriodStart;
+    private LocalDateTime billingPeriodEnd;
+
+    private BigDecimal amount;
+    private String status;
+
+    // entity -> dto
+    public static SubscriptionBillingHistoryInternalDto fromEntity(SubscriptionBillingHistory billingHistory) {
+        return SubscriptionBillingHistoryInternalDto.builder()
+                .billingHistoryId(billingHistory.getId())
+                .billingDate(billingHistory.getBillingDate())
+                .billingPeriodStart(billingHistory.getBillingPeriodStart())
+                .billingPeriodEnd(billingHistory.getBillingPeriodEnd())
+                .amount(billingHistory.getAmount())
+                .status(billingHistory.getStatus())
+                .build();
+    }
+
+    // List<entity> -> List<dto>
+    public static List<SubscriptionBillingHistoryInternalDto> fromEntities(List<SubscriptionBillingHistory> billingHistories) {
+        return billingHistories.stream()
+                .map(SubscriptionBillingHistoryInternalDto::fromEntity)
+                .toList();
+    }
+}
