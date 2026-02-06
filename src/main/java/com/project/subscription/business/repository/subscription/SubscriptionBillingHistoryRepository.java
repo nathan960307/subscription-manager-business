fb4c15d13c046c52ef13f8d9a16277977a974f58
nathan <nathan960307@gmail.com>
2026-02-06 09:18:00 +0900

    feat(subscription): 구독 결제 내역 repository 사용자ID와 구독ID를 이용해서 조회 하는 method 추가(#1) [2026-02-06]

--- src/main/java/com/project/subscription/business/repository/subscription/SubscriptionBillingHistoryRepository.java
+++ src/main/java/com/project/subscription/business/repository/subscription/SubscriptionBillingHistoryRepository.java
@@ -9,10 +9,11 @@ import java.util.List;
 public interface SubscriptionBillingHistoryRepository extends JpaRepository<SubscriptionBillingHistory,Long> {
 
     // 특정 구독의 모든 Billing History를 조회 하여 날짜 순으로 내림차순으로 정렬(최신순)
-    List<SubscriptionBillingHistory> findBySubscriptionIdOrderByBillingDateDesc(Long subscriptionId);
+    List<SubscriptionBillingHistory> findByUserIdAndSubscriptionIdOrderByBillingDateDesc(Long userId, Long subscriptionId);
+
 
     // 과거순(오름차순) 조회
-    List<SubscriptionBillingHistory> findBySubscriptionIdOrderByBillingDateAsc(Long subscriptionId);
+    List<SubscriptionBillingHistory> findByUserIdAndSubscriptionIdOrderByBillingDateAsc(Long userId, Long subscriptionId);
 
 
 }
