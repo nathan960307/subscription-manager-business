6dacb163c37ada70022ac369eee4d169f3e6f047
nathan <nathan960307@gmail.com>
2026-03-21 23:55:46 +0900

    refactor(crud): 구독 서비스 이름 필드 추가 (#15) [2026-03-22]

--- src/main/java/com/project/subscription/business/presentation/subscription/dto/request/SubscriptionCreateRequest.java
+++ src/main/java/com/project/subscription/business/presentation/subscription/dto/request/SubscriptionCreateRequest.java
@@ -11,6 +11,7 @@ import java.math.BigDecimal;
 public class SubscriptionCreateRequest {
 
     private Long serviceId; // 서비스 ID
+    private String serviceName; // 구독 서비스 이름
     private BigDecimal price; // 구독 가격
     private BillingCycle billingCycle; // 주기
 }
