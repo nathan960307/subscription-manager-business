36543433bacb7c1af47178b9302bbef6ec648aad
nathan <nathan960307@gmail.com>
2026-03-21 16:13:28 +0900

    feat(crud): 통계 정보를 담을 internal dto 생성 (#13) [2026-03-21]

---
+++ src/main/java/com/project/subscription/business/presentation/subscription/dto/internal/MonthlyBillingItemDto.java
@@ -0,0 +1,25 @@
+package com.project.subscription.business.presentation.subscription.dto.internal;
+
+import com.project.subscription.business.domain.subscription.entity.Subscription;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Getter
+@Builder
+public class MonthlyBillingItemDto {
+
+    private Long serviceId;
+    private BigDecimal price;
+    private LocalDateTime nextBillingDate; // 결제 예정 일자
+
+    public static MonthlyBillingItemDto fromEntity(Subscription subscription) {
+        return MonthlyBillingItemDto.builder()
+                .serviceId(subscription.getServiceId())
+                .price(subscription.getPrice())
+                .nextBillingDate(subscription.getNextBillingDate())
+                .build();
+    }
+}
