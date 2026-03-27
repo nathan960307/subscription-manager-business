c625c200bdef950ecdc7cfc119249bb1e979444a
nathan <nathan960307@gmail.com>
2026-03-27 15:09:00 +0900

    refactor: 입력 필드 검증값 추가 (#16) [2026-03-27]

--- src/main/java/com/project/subscription/business/presentation/subscription/dto/request/SubscriptionCreateRequest.java
+++ src/main/java/com/project/subscription/business/presentation/subscription/dto/request/SubscriptionCreateRequest.java
@@ -1,6 +1,9 @@
 package com.project.subscription.business.presentation.subscription.dto.request;
 
 import com.project.subscription.business.domain.subscription.entity.BillingCycle;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -11,8 +14,15 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 public class SubscriptionCreateRequest {
 
+    @NotBlank
     private String serviceName; // 구독 서비스 이름
+
+    @NotNull
+    @DecimalMin(value = "0.0", inclusive = false)
     private BigDecimal price; // 구독 가격
+
+    @NotNull
     private BillingCycle billingCycle; // 주기
+
     private LocalDateTime startDate; // 시작일
 }
