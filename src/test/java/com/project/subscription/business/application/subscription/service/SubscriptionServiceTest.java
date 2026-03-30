669af6b250a55f07fe1d5813e1fdb60b13b1824e
nathan <nathan960307@gmail.com>
2026-03-30 12:44:54 +0900

    feat(test) : 구독 서비스 단위 테스트 코드 스켈레톤 코드 생성 #19 [2026-03-29]

---
+++ src/test/java/com/project/subscription/business/application/subscription/service/SubscriptionServiceTest.java
@@ -0,0 +1,24 @@
+package com.project.subscription.business.application.subscription.service;
+
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.context.ActiveProfiles;
+
+@ActiveProfiles("test")
+@ExtendWith(MockitoExtension.class)
+public class SubscriptionServiceTest {
+
+    // mock 객체 선언
+
+
+    // test 대상 주입
+
+
+    // 테스트 메서드
+
+
+
+
+
+}
